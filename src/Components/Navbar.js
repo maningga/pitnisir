29bd9420450d569da24d8d35f9f431c71a489cc1
maningga <kenortiz14@gmail.com>
2024-08-06 22:39:04 +0800

    Updated project with new navbar and sections, added smooth scrolling, and updated styles

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="container">
-        <div className="logo">My Portfolio</div>
+        <div className="logo">My Nigga</div>
         <ul className="nav-links">
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
