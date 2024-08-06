857f0daca9fb13c1c9bfc26d59e366e83c9b58a6
maningga <kenortiz14@gmail.com>
2024-08-06 19:24:14 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+import Projects from './Components/Projects';
+import Skills from './Components/Skills';
+import Contact from './Components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <section id="skills">
+      <h2>My Skills</h2>
+      <p>This is the skills section.</p>
+    </section>
+  );
+};
+
+export default Skills;
